aa36847ffb25e0f9413e796d2ac123b5552f571b
fvera <felipe94b@gmail.com>
2022-12-06 16:46:27 -0300

    agregamos x,y datos tesis

--- tesis/procesamiento_input_paper.py
+++ tesis/procesamiento_input_paper.py
@@ -1,9 +1,8 @@
 import os
 from collections import defaultdict
-from typing import List, Tuple
-import utm
 
 import pandas as pd
+import utm
 
 enlace_input_tesis = "G:\Mi unidad\descarga_chrome\paper_tesis"
 directorio = r"C:\Users\fvera\PycharmProjects\pyOtp"
@@ -64,7 +63,9 @@ def read_consolidado_parada_metro():
     xs, ys, nombres = list(df['stop_lon']), list(df['stop_lat']), list(df['stop_name'])
     for i in range(len(xs)):
         nombre, x, y = nombres[i], xs[i], ys[i]
-        nombre = nombre.upper().replace("Ñ", "N").replace("Á", "A").replace("É", "E").replace("Í", "I").replace("Ó", "O").replace("Ú", "U").replace(" (L3-L1)", "").replace(" (L1-L5)", "")
+        nombre = nombre.upper().replace("Ñ", "N").replace("Á", "A").replace("É", "E").replace("Í", "I").replace("Ó",
+                                                                                                                "O").replace(
+            "Ú", "U").replace(" (L3-L1)", "").replace(" (L1-L5)", "")
         x, y, _, _ = utm.from_latlon(y, x)
         try:
             dic_nombre_xy[nombre] = (float(x), float(y))
@@ -77,10 +78,12 @@ def read_consolidado_parada_metro():
 def join_x_y_paradero_subida_bajada(viajes, dic_paradas, dic_paradas_metro):
     l_ps = list(viajes['paraderosubida'])
     l_pb = list(viajes['paraderobajada'])
+    oxs, oys, oxb, oyb = [], [], [], []
     paraderos_not_match = []
     for i in range(len(viajes)):
         ps = l_ps[i]
         pb = l_pb[i]
+        xs, ys, xb, yb = None, None, None, None
         try:
             xs, ys = dic_paradas[ps]
         except Exception as e:
@@ -88,7 +91,6 @@ def join_x_y_paradero_subida_bajada(viajes, dic_paradas, dic_paradas_metro):
                 xs, ys = dic_paradas_metro[ps]
             except Exception as e:
                 paraderos_not_match.append(ps)
-                continue
         try:
             xb, yb = dic_paradas[pb]
         except Exception as e:
@@ -96,9 +98,19 @@ def join_x_y_paradero_subida_bajada(viajes, dic_paradas, dic_paradas_metro):
                 xb, yb = dic_paradas_metro[pb]
             except Exception as e:
                 paraderos_not_match.append(pb)
-                continue
+        oxs.append(xs)
+        oys.append(ys)
+        oxb.append(xb)
+        oyb.append(yb)
     paraderos_not_match = list(set(paraderos_not_match))
     print(paraderos_not_match)
+    viajes["xs"] = oxs
+    viajes["ys"] = oys
+    viajes["xb"] = oxb
+    viajes["yb"] = oyb
+    viajes = viajes[(viajes['xs'].notna()) & (viajes['ys'].notna()) & (viajes['xb'].notna()) & (viajes['yb'].notna())]
+    print("Numero de viajes con paradero de subida y bajada con coordenadas ", len(viajes))
+    return viajes
 
 
 viajes = read_input_small()
@@ -109,10 +121,9 @@ id_counts = contar_observaciones_id(viajes)
 print(viajes['paraderosubida'])
 print(paradas['Código paradero TS'])
 
-join_x_y_paradero_subida_bajada(viajes, dic_paradas, dic_paradas_metro)
-
+viajes = join_x_y_paradero_subida_bajada(viajes, dic_paradas, dic_paradas_metro)
 
 # graficar OD
-    # ¿buffers?
+# ¿buffers?
 # graficar viajes
-    #
+#
