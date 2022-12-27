d036110e332eca0c510650f700a35b1afac5ad59
Felipe Vera <fvera@transapp.cl>
2022-12-27 11:09:55 -0300

    changes at 27 dic

--- tesis/app_buffersod.py
+++ tesis/app_buffersod.py
@@ -4,11 +4,14 @@ from collections import defaultdict
 import pandas as pd
 import dash_bootstrap_components as dbc
 from dash import html, Input, Output, callback, Dash, State, dcc
-
+import dash_leaflet as dl
 from procesamiento_input_paper import get_viajes_xy_paradas_subidas_bajadas, read_consolidado_parada, \
     read_consolidado_parada_metro
+import logging
 
-viajes = get_viajes_xy_paradas_subidas_bajadas(1000)
+logger_buffer = logging.getLogger()
+
+viajes = get_viajes_xy_paradas_subidas_bajadas(-1)
 # leemos consolidado de paradas
 paradas, dic_paradas = read_consolidado_parada()
 # leemos consolidado de parada de metro
@@ -83,6 +86,9 @@ def read_json_buffers():
     return data
 
 
+buffers = read_json_buffers()
+
+
 def filter_viajes_by_origen(viajes: pd.DataFrame, ps):
     return viajes[viajes['paraderosubida'] == ps]
 
@@ -97,13 +103,12 @@ def filter_viajes_by_origen_and_destino(viajes: pd.DataFrame, ps, pb):
 
 def get_selector_parada(dic_paradas, dic_parada_metro):
     paraderos = list(set(list(dic_paradas.keys()) + list(dic_parada_metro.keys())))
-    return dcc.Dropdown([{'label': p, 'value': p} for p in paraderos], paraderos[0], id='selector_parada_buffers')
+    return dcc.Dropdown([{'label': p, 'value': p} for p in paraderos], 'LOS LEONES', id='selector_parada_buffers')
 
 
 def get_selector_tipo_buffers():
     tipo = ['Origen', 'Destino']
     return dcc.Dropdown([{'label': p, 'value': p} for p in tipo], tipo[1], id='selector_tipo_buffers')
-
 
 
 layout_buffer = html.Div([
@@ -115,24 +120,159 @@ layout_buffer = html.Div([
         dbc.Col(["Tipo"], width=3),
         dbc.Col([get_selector_tipo_buffers()], width=3),
     ]),
-    dbc.Row([
-        dbc.Col(["Numero de viajes"], width=3),
-        dbc.Col(["Numero de origenes/destinos"], width=3),
-    ]),
+    html.Hr(),
     dbc.Row([
         dbc.Col([], width=10),
-        dbc.Col([dbc.Button("Procesar", color="primary", className="me-1", id='procesarbuffer')], width=2),
+        dbc.Col([dbc.Button("Procesar", color="primary", className="me-1", id='procesarbuffer', n_clicks=0)], width=2),
     ]),
-    html.Div(id='map_buffers_od')
+    dbc.Row([
+        dbc.Col(html.Div(id='map_buffers_od'), width=9),
+        dbc.Col([dbc.Row(html.Div(id="numero_viajes_buffer")),
+                 dbc.Row(html.Div(id="numero_od_buffer"))], width=3)
+    ]),
 
 ])
 
 
+def get_buffer(tipo, paradero_central):
+    if tipo == "Origen":
+        color = 'green'
+    else:
+        color = 'red'
+    try:
+        lon, lat = dic_paradas[paradero_central]
+    except:
+        lon, lat = dic_paradas_metro[paradero_central]
+    markers = [dl.Circle(center=(lat, lon), radius=500, color=color, fillOpacity=0.1, dashArray='10,20')]
+    return dl.FeatureGroup(markers)
+
+
+def get_paraderos_buffers(paraderos_buffers, tipo, viajes_buffers, paradero_central):
+    markers = []
+    for paradero in paraderos_buffers:
+        if tipo == "Origen":
+            n_viajes = len(viajes_buffers[viajes_buffers['paraderosubida'] == paradero])
+            color = 'green' if n_viajes != 0 else 'grey'
+        else:
+            n_viajes = len(viajes_buffers[viajes_buffers['paraderobajada'] == paradero])
+            color = 'red' if n_viajes != 0 else 'grey'
+        try:
+            lon, lat = dic_paradas[paradero]
+
+        except:
+            try:
+                lon, lat = dic_paradas_metro[paradero]
+            except:
+                continue
+        markers.append(
+            dl.CircleMarker(
+                center=(lat, lon),
+                radius=5,
+                color=color,
+                # icon=icon,
+                children=[dl.Tooltip(paradero),
+                          dl.Popup([
+                              html.P(
+                                  "Observaciones: {}".format(n_viajes))
+                          ])],
+            )
+        )
+    return dl.FeatureGroup(markers)
+
+
+def get_paraderos_no_buffers(tipo, viajes_buffers):
+    if tipo == "Origen":
+        paraderos = viajes_buffers['paraderobajada'].unique()
+        color = 'red'
+    else:
+        paraderos = viajes_buffers['paraderosubida']
+        color = 'green'
+    markers = []
+    for paradero in paraderos:
+        if tipo == "Origen":
+            n_viajes = len(viajes_buffers[viajes_buffers['paraderobajada'] == paradero])
+        else:
+            n_viajes = len(viajes_buffers[viajes_buffers['paraderosubida'] == paradero])
+        try:
+            lon, lat = dic_paradas[paradero]
+
+        except:
+            try:
+                lon, lat = dic_paradas_metro[paradero]
+            except:
+                continue
+        markers.append(
+            dl.CircleMarker(
+                center=(lat, lon),
+                radius=5,
+                color=color,
+                # icon=icon,
+                children=[dl.Tooltip(paradero),
+                          dl.Popup([
+                              html.P(
+                                  "Observaciones: {}".format(n_viajes))
+                          ])],
+            )
+        )
+    return dl.FeatureGroup(markers)
+
+
+def get_mapa_buffer(viajes_buffers, paraderos_buffer, parada, tipo):
+    marcadores_paraderos_buffer = get_paraderos_buffers(paraderos_buffer, tipo, viajes_buffers, parada)
+    buffers_map = get_buffer(tipo, parada)
+    marcadores_paraderos_no_buffer = get_paraderos_no_buffers(tipo, viajes_buffers)
+    try:
+        lon, lat = dic_paradas[parada]
+    except:
+        lon, lat = dic_paradas_metro[parada]
+    my_map = dl.Map(center=(lat, lon), zoom=15, children=[
+        dl.LayersControl(
+            [
+                dl.BaseLayer(
+                    dl.TileLayer(),
+                    name="Base",
+                    checked=True,
+                ),
+                dl.Overlay(
+                    [buffers_map],
+                    name="Buffer",
+                    checked=True,
+                ),
+                dl.Overlay(
+                    [marcadores_paraderos_buffer],
+                    name="Paraderos en Buffer",
+                    checked=True,
+                ),
+                dl.Overlay(
+                    [marcadores_paraderos_no_buffer],
+                    name="Paraderos no en Buffer",
+                    checked=True,
+                )
+            ],
+        ),
+    ],
+                    style={'width': '100%', 'height': '100vh', 'margin': "auto", "display": "block"}, id="map_buffer")
+    return my_map
+
+
 @callback(
     Output('map_buffers_od', 'children'),
-    Input("procesar", "n_clicks"), State('selector_parada_buffers', 'value'), State('selector_tipo_buffers', 'value')
+    Output('numero_viajes_buffer', 'children'),
+    Output('numero_od_buffer', 'children'),
+    [Input("procesarbuffer", "n_clicks")], State('selector_parada_buffers', 'value'),
+    State('selector_tipo_buffers', 'value')
 )
 def update_output(n, parada, tipo):
-    return 'parada seleccionada {}, {}'.format(parada, tipo)
-
-
+    paraderos_buffers = buffers[parada] + [parada]
+    logger_buffer.info("paradero dentro del buffer")
+    logger_buffer.info(paraderos_buffers)
+    if tipo == "Origen":
+        viajes_parada = viajes[viajes['paraderosubida'].isin(paraderos_buffers)]
+        my_map = get_mapa_buffer(viajes_parada, paraderos_buffers, parada, tipo)
+        return my_map, len(viajes_parada), len(
+            list(viajes_parada['paraderosubida'].unique()))
+    else:
+        viajes_parada = viajes[viajes['paraderobajada'].isin(paraderos_buffers)]
+        my_map = get_mapa_buffer(viajes_parada, paraderos_buffers, parada, tipo)
+        return my_map, len(viajes_parada), len(
+            list(viajes_parada['paraderobajada'].unique()))
